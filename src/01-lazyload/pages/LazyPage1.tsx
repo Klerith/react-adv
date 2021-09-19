f1c25b1bce0d0e6e5d285831ca9a091dd015ad02
Fernando Herrera <fernando.herrera85@gmail.com>
2021-09-19 11:05:04 -0400

    Paginas lazy agregadas

---
+++ src/01-lazyload/pages/LazyPage1.tsx
@@ -0,0 +1,7 @@
+
+
+export const LazyPage1 = () => {
+    return (
+        <h1>LazyPage1</h1>
+    )
+}
---
+++ src/01-lazyload/pages/LazyPage2.tsx
@@ -0,0 +1,7 @@
+
+
+export const LazyPage2 = () => {
+    return (
+        <h1>LazyPage2</h1>
+    )
+}
