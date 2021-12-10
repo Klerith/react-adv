20c3f3ff6d5550926881ce3385d482be1ab7d627
bertini91 <nicolas_bertini@hotmail.com>
2021-12-09 23:09:10 -0300

    lazyload basico listo, enrutamiento v5

---
+++ src/01-lazyload/router/routes.ts
@@ -0,0 +1,33 @@
+import { LazyExoticComponent, lazy } from 'react';
+
+type JSXComponent = () => JSX.Element;
+
+interface Route {
+    path: string;
+    component: LazyExoticComponent<JSXComponent> | JSXComponent,
+    name: string;
+    children?: Route[]
+}
+
+
+const LazyPage1 = lazy( () => import(/* webpackChunkName: "LazyPage1" */'../pages/LazyPage1') );
+const LazyPage2 = lazy( () => import(/* webpackChunkName: "LazyPage2" */'../pages/LazyPage2') );
+const LazyPage3 = lazy( () => import(/* webpackChunkName: "LazyPage3" */'../pages/LazyPage3') );
+
+export const routes: Route[] = [
+    {
+        path: '/lazy1',
+        component: LazyPage1,
+        name: 'LazyPage-1'
+    },
+    {
+        path: '/lazy2',
+        component: LazyPage2,
+        name: 'LazyPage-2'
+    },
+    {
+        path: '/lazy3',
+        component: LazyPage3,
+        name: 'LazyPage-3'
+    },
+]
