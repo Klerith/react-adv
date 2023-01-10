a2f7c6abeae0228b1ff90f802381b21e8d87e102
juliang12 <jgarcia.e16@gmail.com>
2023-01-10 09:51:46 -0300

    rutas anidadas listas

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -8,10 +8,16 @@ interface Routes {
   name: string;
 }
 
+const LazyLayout = lazy(() => import("../01-layload/layout/LazyLayout"));
 const Lazy1 = lazy(() => import("../01-layload/pages/LazyPage1"));
 const Lazy2 = lazy(() => import("../01-layload/pages/LazyPage2"));
 
 export const routes: Routes[] = [
-  { to: "/lazy1", path: "lazy1", Component: Lazy1, name: "Lazy-1" },
+  {
+    to: "/lazyload/",
+    path: "/lazyload/*",
+    Component: LazyLayout,
+    name: "Lazy-1",
+  },
   { to: "/lazy2", path: "lazy2", Component: Lazy2, name: "Lazy-2" },
 ];
