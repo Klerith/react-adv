5abe018d0520c80e17bbfe7d43b99f5065c2eee2
juliang12 <jgarcia.e16@gmail.com>
2023-01-10 00:24:19 -0300

    Componentes Lazy agregados

---
+++ src/01-layload/pages/LazyPage1.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LazyPage1 = () => {
+  return <div>LazyPage1</div>;
+};
+
+export default LazyPage1;
---
+++ src/01-layload/pages/LazyPage2.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LazyPage2 = () => {
+  return <div>LazyPage2</div>;
+};
+
+export default LazyPage2;
