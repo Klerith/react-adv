2f73b4d1ba5cc2f9dce2507e4ee25b6dcbcd36fe
Cristian <desarrollo04@promotec.com.co>
2023-06-23 14:41:35 -0600

    Se suben cambios

--- src/components/ProductButtons.tsx
+++ src/components/ProductButtons.tsx
@@ -3,13 +3,9 @@ import { useContext } from "react";
 import styles from "../styles/styles.module.css";
 import { ProductContext } from "./ProductCard";
 
-
-
 export const ProductButtons = () => {
-
-    //forma de pasar funciones es con el useContext
-    const { counter, increaseBy } = useContext(ProductContext);
-    
+  //forma de pasar funciones es con el useContext
+  const { counter, increaseBy } = useContext(ProductContext);
 
   return (
     <div className={styles.buttonsContainer}>
