b5ed6e87476aef82232249f9ecce19db690849f0
Cristian <desarrollo04@promotec.com.co>
2023-06-27 09:03:37 -0600

    create file cess new

--- src/pages/ShoppingPage.tsx
+++ src/pages/ShoppingPage.tsx
@@ -7,28 +7,35 @@ import {
   ProductTitle,
 } from "../components/index";
 
+import  '../styles/styles.module.css'
+
+
+
+
+
 const product = {
   id: "01",
   title: "Coffie -img",
   img: "../assets/coffee-mug.png",
 };
 
+
 export const ShoppingPage = () => {
   return (
-    <div>
-      <h1>Shopping page</h1>
+    <div  className="bg-dark">
+      <h1>Shopping page </h1>
       <hr />
       <div style={{ display: "flex", flexDirection: "row", flexWrap: "wrap" }}>
         {/* HOC = ESTO QUIERE DECIR QUE RECIBE HIJOS   */}
         <ProductCard product={product}>
-          <ProductCard.Image />
+          <ProductCard.Image   />
           <ProductCard.Title title="Cofie" />
           <ProductCard.Buttons />
         </ProductCard>
 
-        <ProductCard product={product}>
+        <ProductCard product={product} >
           <ProductImage />
-          <ProductTitle title="Hola mundo" />
+          <ProductTitle />
           <ProductButtons />
         </ProductCard>
       </div>
